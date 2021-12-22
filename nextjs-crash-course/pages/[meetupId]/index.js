4293f5173eaf27edf58c17a369b9ff6e42a2bdf8
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-12-22 19:48:12 +0530

    add home, new meeting and meeting details view

---
+++ nextjs-crash-course/pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <div>
+      <MeetupDetail
+        image="https://corporette.com/wp-content/uploads/2017/03/best-places-for-informal-business-meetings-for-women-lawyers-consultants-and-more.jpg"
+        title="A First Meetup"
+        address="address"
+        description="description"
+      ></MeetupDetail>
+    </div>
+  );
+}
+
+export default MeetupDetails;
--- nextjs-crash-course/pages/news/[newsId].js
+++
@@ -1,5 +0,0 @@
-function DetailPage() {
-  return <h1>Detail Page</h1>;
-}
-
-export default DetailPage;
