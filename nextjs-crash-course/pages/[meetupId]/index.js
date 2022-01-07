2feb7300c55d456dd1abc18d784df6b5de31bce9
shalithadeshan <shalithad8520jayasekara@gmail.com>
2022-01-07 22:26:01 +0530

    add getStaticPaths and getStaticProps

--- nextjs-crash-course/pages/[meetupId]/index.js
+++ nextjs-crash-course/pages/[meetupId]/index.js
@@ -8,9 +8,46 @@ function MeetupDetails() {
         title="A First Meetup"
         address="address"
         description="description"
-      ></MeetupDetail>
+      ></MeetupDetail>{" "}
     </div>
   );
+}
+
+// this function should add if page dynamic and using getStaticProps
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "1",
+        },
+      },
+      {
+        params: {
+          meetupId: "2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  console.log("meetupId", meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://corporette.com/wp-content/uploads/2017/03/best-places-for-informal-business-meetings-for-women-lawyers-consultants-and-more.jpg",
+        title: "A First Meetup",
+        address: "address",
+        description: "description",
+        id: "1",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- nextjs-crash-course/pages/index.js
+++ nextjs-crash-course/pages/index.js
@@ -30,12 +30,25 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// this will run on the server after deployment (every incoming request)
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: dummyMeetups,
+//     },
+//   };
+// }
 // this function is called when the page is loaded not component function
+// also this will run in the build process
 export async function getStaticProps() {
   return {
     props: {
       meetups: dummyMeetups,
     },
+    revalidate: 10, // this will regenerate the page every 10 seconds in the server
   };
 }
 
