4ece9921baf011ed7092782f5ad57edb61d608bc
shalithadeshan <shalithad8520jayasekara@gmail.com>
2022-01-15 19:29:01 +0530

    big fix

--- nextjs-crash-course/components/meetups/MeetupDetail.js
+++ nextjs-crash-course/components/meetups/MeetupDetail.js
@@ -1,9 +1,9 @@
 import classes from "./MeetupDetail.module.css";
-
+import Image from "next/image";
 function MeetupDetail(props) {
   return (
     <div className={classes.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
